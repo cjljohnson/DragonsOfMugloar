3a1f2d6e2e921683106dde089b8a5db7eb7ea8d8
Chris Johnson <cjljohnson@gmail.com>
2016-08-14 21:52:36 +0100

    Add functionality to GET knight JSON string from server

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 
 public class Main {
 	public static void main(String[] args) {
-		String knightJSON = "{\"gameId\":2321046,\"knight\":{\"name\":\"Sir. Sam Romero of Yukon\",\"attack\":3,\"armor\":8,\"agility\":4,\"endurance\":5}}";
-		knightJSON = "{\"gameId\":1170606,\"knight\":{\"name\":\"Sir. Peter Lowe of Newfoundland and Labrador\",\"attack\":7,\"armor\":3,\"agility\":6,\"endurance\":4}}";
-		Knight knight = Utils.extractKnightFromJson(knightJSON);
+		//String knightJSON = "{\"gameId\":2321046,\"knight\":{\"name\":\"Sir. Sam Romero of Yukon\",\"attack\":3,\"armor\":8,\"agility\":4,\"endurance\":5}}";
+		//knightJSON = "{\"gameId\":1170606,\"knight\":{\"name\":\"Sir. Peter Lowe of Newfoundland and Labrador\",\"attack\":7,\"armor\":3,\"agility\":6,\"endurance\":4}}";
+		//Knight knight = Utils.extractKnightFromJson(knightJSON);
+		
+		Knight knight = Utils.fetchKnight();
+		
 		System.out.println(knight);
-		System.out.println(knight.getAttributePosition(Knight.ATTACK));
-		System.out.println(knight.getAttributePosition(Knight.ARMOR));
-		System.out.println(knight.getAttributePosition(Knight.AGILITY));
-		System.out.println(knight.getAttributePosition(Knight.ENDURANCE));
+//		System.out.println(knight.getAttributePosition(Knight.ATTACK));
+//		System.out.println(knight.getAttributePosition(Knight.ARMOR));
+//		System.out.println(knight.getAttributePosition(Knight.AGILITY));
+//		System.out.println(knight.getAttributePosition(Knight.ENDURANCE));
 		Dragon dragon = DragonCreator.createDragonNormal(knight);
 		System.out.println(dragon);
 	}
--- src/Utils.java
+++ src/Utils.java
@@ -1,10 +1,95 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Utils {
 
+	public static Knight fetchKnight() {
+		// Create URL for knight get page
+		URL url = null;
+		try {
+			url = new URL("http://www.dragonsofmugloar.com/api/game");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// Perform HTTP request to the URL and receive a JSON response back
+		String jsonResponse = null;
+		try {
+			jsonResponse = makeHttpRequest(url);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// Extract info from JSON response and create a knight object
+		Knight knight = extractKnightFromJson(jsonResponse);
+		
+		return knight;
+	}
+	
+	public static String makeHttpRequest(URL url) throws IOException {
+		String jsonResponse = "";
+		
+		// If the URL is null, return early
+		if (url == null) {
+			return jsonResponse;
+		}
+		
+		HttpURLConnection urlConnection = null;
+		InputStream inputStream = null;
+		try {
+			urlConnection = (HttpURLConnection) url.openConnection();
+			urlConnection.setReadTimeout(10000);
+			urlConnection.setConnectTimeout(15000);
+			urlConnection.setRequestMethod("GET");
+			urlConnection.connect();
+			
+			// If connection successful (200), read input stream and parse response
+			if (urlConnection.getResponseCode() == 200) {
+				inputStream = urlConnection.getInputStream();
+				jsonResponse = readFromStream(inputStream);
+			} else {
+				System.out.println("Error response code: " + urlConnection.getResponseCode());
+			}
+		} catch (IOException e) {
+			    System.out.println("Problem retrieving knight JSON results");
+		} finally {
+			if (urlConnection != null) {
+				urlConnection.disconnect();
+			}
+			if (inputStream != null) {
+				inputStream.close();
+			}
+		}
+		return jsonResponse;
+	}
+	
+	
+	// Convert inputStream into String that contains whole JSON response
+	private static String readFromStream(InputStream inputStream) throws IOException {
+		StringBuilder output = new StringBuilder();
+		if (inputStream != null) {
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+			BufferedReader reader = new BufferedReader(inputStreamReader);
+			String line = reader.readLine();
+			while (line != null) {
+				output.append(line);
+				line = reader.readLine();
+			}
+		}
+		return output.toString();
+	}
 	
 	public static Knight extractKnightFromJson(String knightJSON) {
 		// If JSON string is empty or null, return early
