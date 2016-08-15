8ebd8066139127e6e08ce3c5bcbce45fb3416ae8
Chris Johnson <cjljohnson@gmail.com>
2016-08-15 04:15:58 +0100

    Add prompt for number of iterations

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,61 @@
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		// String knightJSON = "{\"gameId\":2321046,\"knight\":{\"name\":\"Sir.
-		// Sam Romero of
-		// Yukon\",\"attack\":3,\"armor\":8,\"agility\":4,\"endurance\":5}}";
-		// knightJSON = "{\"gameId\":1170606,\"knight\":{\"name\":\"Sir. Peter
-		// Lowe of Newfoundland and
-		// Labrador\",\"attack\":7,\"armor\":3,\"agility\":6,\"endurance\":4}}";
-		// Knight knight = Utils.extractKnightFromJson(knightJSON);
 
 		int battles = 0;
 		int victories = 0;
+		int iterations = 100;
 		int i = 0;
-		while (i < 100) {
+		
+		System.out.print("Number of iterations: ");
+		Scanner reader = new Scanner(System.in);
+		iterations = reader.nextInt();
+		
+		
+		// Loop through battles
+		while (i < iterations) {
 			// Fetch a new knight from server
 			Knight knight = Utils.fetchKnight();
 
 			// Check weather for battle
 			String weatherCode = Utils.checkWeather(knight.getId());
 					
-			//System.out.println(knight);
+			// Create dragon for battle
 			Dragon dragon = DragonCreator.createDragon(knight, weatherCode);
-			//System.out.println(dragon);
-			//System.out.println(Utils.writeJsonStringFromDragon(dragon));
 
+			// Put dragon to server and get results
+			// If weather is stormy no dragon is sent
 			String result = null;
 			if (!weatherCode.equals("SRO")) {
 				result = Utils.putDragon(dragon);
 			} else {
 				result = Utils.dontSendDragon(dragon);
 			}
-			//System.out.println(result);
 
+			// Log victories and print defeats
 			if (result.contains("Victory")) {
 				victories++;
 			} else {
 				System.out.println(knight);
 				System.out.println(dragon);
 				System.out.println(weatherCode);
-				System.out.println(result);
+				
 			}
+			System.out.println(result);
 			battles++;
 			i++;
 		}
 		System.out.println("Battles: " + battles);
 		System.out.println("Victories: " + victories);
+		
+		// Calculate win percentage
+		double percentage;
+		if (battles > 0) {
+			percentage = (victories / battles) * 100.0;
+		} else {
+			percentage = 0;
+		}
+		System.out.println("Win rate: " + percentage + "%");
 	}
 }
